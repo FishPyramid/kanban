c752d1ba8fdde80ba430520dbd8b502ca0a7fa04
Aidan Saunders <hihihican@gmail.com>
2026-05-02 19:39:49 -0400

    capitalization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 export default function Page() {
   return (
-    <h1 style={{userSelect: 'none'}} className="text-5xl text-center font-bold text-shadow-amber-50 text-shadow-lg/80">My tasks</h1>
+    <h1 style={{userSelect: 'none'}} className="text-5xl text-center font-bold text-shadow-amber-50 text-shadow-lg/80">My Tasks</h1>
   )
 }
